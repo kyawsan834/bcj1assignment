f2bfcb2aa12e4eaefd662dc7d73c943104081eac
kyawsan84yay <kyawsan84yay@gmail.com>
2023-09-07 21:58:42 +0630

    07 09 2023 9:57PM

--- js/app.js
+++ js/app.js
@@ -1,72 +1,64 @@
+/** @format */
+
 // console.log("hi");
 
 // jQuery Area
-($ => {
+(($) => {
+  // console.log("hay");
 
-    // console.log("hay");
+  // Start preloader
 
-    // Start preloader
+  $(window).on("load", function () {
+    $(".preloader").fadeOut(1000);
+  });
 
-    $(window).on("load",function(){
-        $(".preloader").fadeOut(1000);
-    });
+  // End preloader
 
-    // End preloader
+  // for burger
+  $(".navbuttons").click(function () {
+    $(".navbuttons").toggleClass("crossxs");
+  });
 
-    // for burger
-    $(".navbuttons").click(function () {
-        $(".navbuttons").toggleClass("crossxs");
-    });
+  // for nav
+  $(window).scroll(function () {
+    let getscrolltop = $(this).scrollTop();
+    // console.log(getscrolltop);
 
-    // for nav
-    $(window).scroll(function () {
-        let getscrolltop = $(this).scrollTop();
-        // console.log(getscrolltop);
+    if (getscrolltop >= 280) {
+      $(".navbs").addClass("navbsbgs");
+    } else {
+      $(".navbs").removeClass("navbsbgs");
+    }
+  });
 
-        if (getscrolltop >= 280) {
-            $(".navbs").addClass("navbsbgs");
-        } else {
-            $(".navbs").removeClass("navbsbgs");
+  // Start Our Project Section
 
-        }
-    });
+  $(".projectlists").click(function () {
+    // for active
+    $(this).addClass("activeitems").siblings().removeClass("activeitems");
 
+    let getattr = $(this).attr("data-filter");
+    console.log(getattr);
 
+    // for filters
+    if (getattr == "all") {
+      $(".filters").show("slide", 350);
+    } else {
+      $(".filters").hide("slide", 350);
+      $(".filters")
+        .not("." + getattr)
+        .hide("slide", 350);
+      $(".filters")
+        .filter("." + getattr)
+        .show("slide", 350);
+    }
+  });
 
-    // Start Our Project Section
+  // End Our Project Section
 
-    $(".projectlists").click(function () {
+  let date = new Date();
+  const getFullYear = date.getFullYear();
+  console.log(getFullYear);
 
-        // for active
-        $(this).addClass("activeitems").siblings().removeClass("activeitems");
-
-        let getattr = $(this).attr("data-filter");
-        console.log(getattr);
-
-        // for filters
-        if (getattr == "all") {
-            $(".filters").show("slide", 350);
-        } else {
-
-            $(".filters").hide("slide", 350);
-            $(".filters").not("." + getattr).hide("slide", 350);
-            $(".filters").filter("." + getattr).show("slide", 350);
-
-        }
-
-
-    });
-
-
-
-    // End Our Project Section
-
-
-
-
-
-
-
-
-
+  $("#year").text(getFullYear);
 })(jQuery);
